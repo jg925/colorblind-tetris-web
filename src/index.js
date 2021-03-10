6dffec13d24ec6a747dd49fe1d81aa13eef47002
jg925 <jg925@cornell.edu>
2021-03-09 19:32:26 -0500

    hello world compiled js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var data_js_1 = require("./data.js");
+alert(data_js_1.data.message);
+//# sourceMappingURL=index.js.map
